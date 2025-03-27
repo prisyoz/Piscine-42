10db5ff8b0cebc42ac5b301d1579ad1bfcac192b
Prisyoz <shihui316@gmail.com>
2025-03-27 18:36:46 +0800

    ex02 modified:

--- c08/ex02_ft_abs.h
+++ c08/ex02_ft_abs.h
@@ -2,17 +2,10 @@
 #ifndef FT_ABS_H
 # define FT_ABS_H
 
-# define ABS(Value) 
-
 int	abs_value(int Value)
 {
-	int	i;
-
-	i = -1;
 	if ((Value) < 0)
-	{
-		(Value) *= -1;
-	}
+		return (-Value);
 	return (Value);
 }
 
