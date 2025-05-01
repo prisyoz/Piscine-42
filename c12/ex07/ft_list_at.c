33c1bc131fe5cb5c36c28d1fe15d2e695f96ec0e
Prisyoz <shihui316@gmail.com>
2025-05-01 21:30:47 +0800

    ex07

--- c12/ex07/ft_list_at.c
+++ c12/ex07/ft_list_at.c
@@ -10,7 +10,7 @@ t_list *ft_list_at(t_list *begin_list, unsigned int nbr)
 
     while (begin_list && count < nbr)
     {
-        begin_list = begin_list=>next;
+        begin_list = begin_list->next;
     }
     return (begin_list);
 }
